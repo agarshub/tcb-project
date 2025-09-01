f464b9393ca7193e52789b228bcf9f2b3fff5655
agarshub <shubhamagarwal0111@gmail.com>
2025-09-01 22:11:11 +0530

    Adding 1.c

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n;
+    printf("ENter the value of n\n");
+    scanf("%d",&n);
+    printf("The value of n is %d\n",n);
+    return 0;
+}
